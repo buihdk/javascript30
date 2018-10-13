0e96a2a3b57f6c0ae905e07143fed6d5d58d4656
Khoa Bui <buihdk@gmail.com>
2018-10-14 02:05:31 +0700

    update day 5

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Thanks [Anastasia](https://github.com/AnastasiaVays) for pushing me to do this t
 * Day 2: [CSS + JS Clock](https://www.buihdk.com/javascript30/js-and-css-clock/index.html)
 * Day 3: [CSS Variables](https://www.buihdk.com/javascript30/css-variables/index.html)
 * Day 4: [Array Cardio Day 1](https://www.buihdk.com/javascript30/array-cardio-day-1/index.html)
+* Day 5: [Flex Panel Gallery](https://www.buihdk.com/javascript30/flex-panel-gallery/index.html)
---
+++ flex-panel-gallery/script.js
@@ -0,0 +1,12 @@
+function toggleOpen(e) {
+  // fix double-click and triple-click issue
+  if (!e.detail || e.detail == 1) this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+  if (e.propertyName.includes('flex')) this.classList.toggle('open-active');
+}
+
+const panels = document.querySelectorAll('.panel');
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
