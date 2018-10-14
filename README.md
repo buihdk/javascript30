e9eaa690007aefcbe9ac5f14cb01f84f73d11829
Khoa Bui <buihdk@gmail.com>
2018-10-15 01:33:40 +0700

    update day 6

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Thanks [Anastasia](https://github.com/AnastasiaVays) for pushing me to do this t
 * Day 3: [CSS Variables](https://www.buihdk.com/javascript30/css-variables/index.html)
 * Day 4: [Array Cardio Day 1](https://www.buihdk.com/javascript30/array-cardio-day-1/index.html)
 * Day 5: [Flex Panel Gallery](https://www.buihdk.com/javascript30/flex-panel-gallery/index.html)
+* Day 6: [Ajax Type Ahead](https://www.buihdk.com/javascript30/ajax-type-ahead/index.html)
---
+++ ajax-type-ahead/script.js
@@ -0,0 +1,74 @@
+function findMatches(wordToMatch, cities) {
+  return cities.filter(place => {
+    // here we need to figure out if the city or state matches what was searched
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function displayMatches() { 
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray.map(place => {
+    const regex = new RegExp(this.value, 'gi');
+    const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+    place.difference = place.difference.toString();
+    const nearestDistance = place.difference.replace(regex, `<span class="hl">${this.value}</span>`);
+    const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+    return `
+      <li>
+        <span class='name'>${cityName}, ${stateName}</span>
+        <div class='info'>
+          <span class='distance'>Distance: ${nearestDistance} KM</span> 
+          <span class='population'>Population: ${numberWithCommas(place.population)}</span> 
+        </div>
+      </li>
+    `;
+  }).join('');
+  suggestions.innerHTML = html;
+}
+
+function pythagorasEquirectangular(lat1, lon1, lat2, lon2) {
+  lat1 = lat1 * Math.PI / 180;
+  lat2 = lat2 * Math.PI / 180;
+  lon1 = lon1 * Math.PI / 180;
+  lon2 = lon2 * Math.PI / 180;
+  let R = 6371; // km
+  let dist_x = (lon2 - lon1) * Math.cos((lat1 + lat2) / 2);
+  let dist_y = (lat2 - lat1);
+  let distance = Math.sqrt(dist_x * dist_x + dist_y * dist_y) * R;
+  return distance;
+}
+
+function addDifference(latitude, longitude) {
+  for (let i = 0; i < cities.length; i++) {
+    let difference = pythagorasEquirectangular(latitude, longitude, cities[i].latitude, cities[i].longitude);
+    cities[i].difference = Math.round(difference);
+  }
+  return cities;
+}
+
+let cities = [];
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
+
+navigator.geolocation.getCurrentPosition((currentLocation) => {
+  alert('Fetching cities...')
+  fetch('https://gist.githubusercontent.com/buihdk/43f3e361ec8a1e3208a46537a9529ee1/raw/7027f5708cc267e9b6ed6a5c6d237ca1c485a41f/cities.json')
+  .then(blob => blob.json())
+  .then(data => cities.push(...data))
+  .then(() => alert('Sorting cities based on nearest distances...'))
+  .then(() => addDifference(currentLocation.coords.latitude, currentLocation.coords.latitude))
+  .then(() => cities.sort((city1, city2) => {
+    return (city1.difference > city2.difference) ? 1 : -1;
+  }))
+  .then(() => alert('You can now start your searches...'))
+});
+
