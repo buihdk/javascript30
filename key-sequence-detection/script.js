068fcdc62f8368c469a173a6e3d20e75ae5a8930
Khoa Bui <buihdk@gmail.com>
2018-10-20 16:36:07 +0700

    update day 12

--- key-sequence-detection/script.js
+++ key-sequence-detection/script.js
@@ -1,11 +1,11 @@
 const pressed = [];
-const konamiCode = 'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightab';
+const konamiCode = 'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightba';
 const hiddenText = document.querySelector('.hidden-text');
 
-window.addEventListener('keyup', (e) => {
+window.addEventListener('keyup', function(e) {
   pressed.push(e.key);
-  pressed.splice(-11, pressed.length - 10); // -konamiCode.length - 1, pressed.length - konamiCode.length
-  if(pressed.join('').includes(konamiCode)) {
+  if (pressed.length > 10) pressed.shift();
+  if (pressed.join('').includes(konamiCode)) {
     cornify_add();
     hiddenText.style.visibility = 'visible';
     setTimeout(() => { hiddenText.style.visibility = 'hidden' }, 1000);
