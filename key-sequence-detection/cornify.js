3eb0ce2809479f8b4e6a5dc7c501ff9bb5111a6b
Khoa Bui <buihdk@gmail.com>
2018-10-20 17:17:43 +0700

    fix cornify.js in day 12

--- key-sequence-detection/cornify.js
+++ key-sequence-detection/cornify.js
@@ -1,5 +1,5 @@
-var cornify_count = 0;
-var cornify_add = function() {
+let cornify_count = 0;
+let cornify_add = function() {
 	cornify_count += 1;
 	var cornify_url = 'https://www.cornify.com/';
 	var div = document.createElement('div');
