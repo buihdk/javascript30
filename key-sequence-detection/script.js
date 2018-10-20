2187952396ba76fb8de261e4466624104fa6e038
Khoa Bui <buihdk@gmail.com>
2018-10-20 17:15:10 +0700

    fix cornify.js in  day 12

--- key-sequence-detection/script.js
+++ key-sequence-detection/script.js
@@ -2,12 +2,14 @@ const pressed = [];
 const konamiCode = 'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightba';
 const hiddenText = document.querySelector('.hidden-text');
 
-window.addEventListener('keyup', function(e) {
+window.addEventListener('keyup', (e) => {
   pressed.push(e.key);
+
   if (pressed.length > 10) pressed.shift();
+
   if (pressed.join('').includes(konamiCode)) {
-    cornify_add();
     hiddenText.style.visibility = 'visible';
     setTimeout(() => { hiddenText.style.visibility = 'hidden' }, 1000);
+    cornify_add();
   }
 })
